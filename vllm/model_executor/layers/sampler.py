890316dd2cafdae3ed2c7564f0eb551d8ce956e6
Duyi-Wang <duyi.wang@intel.com>
2024-07-24 15:42:49 +0800

    [Sampler] Optimize top k.

--- vllm/model_executor/layers/sampler.py
+++ vllm/model_executor/layers/sampler.py
@@ -287,31 +287,20 @@ def _apply_top_k_top_p(
     p: torch.Tensor,
     k: torch.Tensor,
 ) -> torch.Tensor:
-    logits_sort, logits_idx = logits.sort(dim=-1, descending=False)
-
-    # Apply top-k.
-    top_k_mask = logits_sort.size(1) - k.to(torch.long)
-    # Get all the top_k values.
-    top_k_mask = logits_sort.gather(1, top_k_mask.unsqueeze(dim=1))
-    top_k_mask = logits_sort < top_k_mask
-    logits_sort.masked_fill_(top_k_mask, -float("inf"))
-
-    # Apply top-p.
-    probs_sort = logits_sort.softmax(dim=-1)
-    probs_sum = probs_sort.cumsum(dim=-1)
-    top_p_mask = probs_sum <= 1 - p.unsqueeze(dim=1)
-    # at least one
-    top_p_mask[:, -1] = False
-    logits_sort.masked_fill_(top_p_mask, -float("inf"))
-
-    # Re-sort the probabilities.
-    src = torch.arange(logits_idx.shape[-1],
-                       device=logits_idx.device).expand_as(logits_idx)
-    logits_idx_inv = torch.empty_like(logits_idx).scatter_(dim=-1,
-                                                           index=logits_idx,
-                                                           src=src)
-    logits = torch.gather(logits_sort, dim=-1, index=logits_idx_inv)
-    return logits
+    result = torch.full_like(logits, float('-inf'))
+    for i in range(logits.size(0)):
+        # Apply top-k
+        logits_topk, logits_idx = torch.topk(logits[i], k[i])
+        # Apply top-p
+        probs_topk = logits_topk.softmax(dim=-1)
+        probs_sum = probs_topk.cumsum(dim=-1)
+        index = torch.searchsorted(probs_sum, p[i], right=True).item()
+        # at least one
+        if index == 0:
+            index = 1
+        logits_idx = logits_idx[:index]
+        result[i][logits_idx] = logits[i][logits_idx]
+    return result
 
 
 def _apply_min_p(
